16b3422ededb9c0a6d185fd8e34fee002ab0fb5b
Massick <4ndyraul@gmail.com>
2022-12-27 22:05:44 -0500

    add OPENAI_API_KEY to config

--- config/constants.ts
+++ config/constants.ts
@@ -1,3 +1,4 @@
 export const token = process.env.TOKEN ?? ''
 export const domain = String(process.env.NEXT_PUBLIC_DOMAIN ?? '').replace(/^http(s)?:\/\//, "")
 export const adminId = process.env.ADMIN_ID ?? ''
+export const OPENAI_API_KEY = process.env.OPENAI_API_KEY ?? ''
